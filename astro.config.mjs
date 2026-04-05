e57ddc819d6781f2dddddc9768fa3c6163c29180
Mhmd0Mhmod <mohamedmahmoudf.2002@gmail.com>
2026-04-05 03:35:48 +0200

    style: standardize code formatting and improve consistency across components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import vercel from '@astrojs/vercel';
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-  adapter: vercel()
+  adapter: vercel(),
 });
